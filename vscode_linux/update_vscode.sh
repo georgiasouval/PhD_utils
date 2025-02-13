7959fa1c6bae797cb710ce04308cf81e7b59c188
ginasouv <georgiasouval@gmail.com>
2025-02-13 12:21:32 +0000

    Initial commit

--- vscode_linux/update_vscode.sh
+++ vscode_linux/update_vscode.sh
@@ -1,10 +1,23 @@
-wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+#!/bin/bash
+
 mkdir -p $HOME/vscode
-dpkg-deb -x vscode.deb $HOME/vscode
-dpkg-deb --control vscode.deb $HOME/vscode/DEBIAN
+mkdir -p $HOME/bin  # Ensure the bin directory exists
+
+# Extract the .deb package
+dpkg-deb -x /home/souval_g_WMGDS.WMG.WARWICK.AC.UK/Downloads/code_1.97.1-1739197498_amd64.deb $HOME/vscode
+dpkg-deb --control /home/souval_g_WMGDS.WMG.WARWICK.AC.UK/Downloads/code_1.97.1-1739197498_amd64.deb $HOME/vscode/DEBIAN
+
+# Create a script to run VS Code
 echo '#!/bin/bash' > $HOME/bin/code
 echo 'exec $HOME/vscode/usr/share/code/code --no-sandbox "$@"' >> $HOME/bin/code
+
+# Make it executable
 chmod +x $HOME/bin/code
+
+# Add to PATH
 echo 'export PATH=$HOME/bin:$PATH' >> $HOME/.bashrc
 source $HOME/.bashrc
+hash -r  # Refresh shell command lookup
+
+# Run VS Code
 code
